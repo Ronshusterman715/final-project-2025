a772c94e781ae7875925068b325e7c75f71a8909
Ronshusterman715 <ron7892123@gmail.com>
2025-09-01 17:19:12 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectToDB = require('./DB/dbService');
 const chalk = require('chalk');
 const router = require('./router/router');
+const { handleError } = require('./utils/handleErrors');
+const corsMiddleware = require('./middlewares/cors');
 
 require("dotenv").config();
 
@@ -10,15 +12,22 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url} | Method: ${req.method} | Time: ${new Date()}`);
+    next();
+});
+
+app.use(corsMiddleware);
+
 app.use(router);
 
 app.use((err, req, res, next) => {
     console.log(err);
-    res.status(500);
-})
+    return handleError(res, 500, "Internal Server Error")
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(chalk.green.bold.bgYellow("app is listening to port " + (process.env.PORT || PORT)));
     connectToDB();
-})
+});
 
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,9 @@
+const cors = require('cors');
+
+const corsMiddleware = cors({
+    origin: [
+        "http://localhost:5173",
+    ]
+});
+
+module.exports = corsMiddleware;
