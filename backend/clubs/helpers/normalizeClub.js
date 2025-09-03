d8f33f45480480fadc5e39eb5d50eb878af2c2b2
Ronshusterman715 <ron7892123@gmail.com>
2025-09-03 20:50:01 +0300

    add todo

--- backend/clubs/helpers/normalizeClub.js
+++ backend/clubs/helpers/normalizeClub.js
@@ -3,6 +3,7 @@ const normalizeClub = async (rawClub, userId) => {
     return {
         ...rawClub,
         image: {
+            //TODO: change default image when decide which club
             url: rawClub.image_url || "/images/ChatGPT-general-club-Image.png",
             alt: rawClub.image_alt || `${rawClub.title} club image`,
         },
