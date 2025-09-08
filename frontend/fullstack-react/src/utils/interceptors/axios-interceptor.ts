5b09b0b6bd430c800cf90f8bb0407a768b273d53
Ronshusterman715 <ron7892123@gmail.com>
2025-09-08 15:16:48 +0300

    start frontend + axios interceptor

---
+++ frontend/fullstack-react/src/utils/interceptors/axios-interceptor.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL_API: string = import.meta.env.VITE_BASE_URL_API;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL_API,
+  timeout: 20000,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["x-auth-token"] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    if (err.response && err.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default axiosInstance;
