a6c8b628f5f161e1557239aa827ccd7cf830e77b
Ronshusterman715 <ron7892123@gmail.com>
2025-09-10 13:16:47 +0300

    fix alt in normalizeclub

--- backend/clubs/helpers/normalizeClub.js
+++ backend/clubs/helpers/normalizeClub.js
@@ -5,7 +5,7 @@ const normalizeClub = (rawClub, userId) => {
         image: {
             //TODO: change default image when decide which club
             url: rawClub.image.url || "/images/chatgpt-general-club-Image.png",
-            alt: rawClub.image.alt || `${rawClub.title} club image`,
+            alt: rawClub.image.alt || `${rawClub.name} club image`,
         },
         userId: rawClub.user_id || userId,
     }
