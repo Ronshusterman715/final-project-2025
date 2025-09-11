2268af5662212920b952e6773009d739d67e5959
Ronshusterman715 <ron7892123@gmail.com>
2025-09-11 14:53:00 +0300

    fix auth interface import

--- frontend/fullstack-react/src/services/authService.ts
+++ frontend/fullstack-react/src/services/authService.ts
@@ -1,6 +1,7 @@
-import axiosInstance from "../utils/interceptors/axios-interceptor"
+import type { Auth } from "../interfaces/Auth/Auth";
+import axiosInstance from "../utils/interceptors/axios-interceptor";
 
 //Login user
-export async Function loginUser(auth: Auth) {
-    return await axiosInstance.post("/users/login", auth)
-};
+export async function loginUser(auth: Auth) {
+  return await axiosInstance.post("/users/login", auth);
+}
