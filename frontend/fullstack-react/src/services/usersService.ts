0654aad8159f041a0c81e9f9cf302ba022e85251
Ronshusterman715 <ron7892123@gmail.com>
2025-09-13 14:26:51 +0300

    add react-toastify success Message + error Message

--- frontend/fullstack-react/src/services/usersService.ts
+++ frontend/fullstack-react/src/services/usersService.ts
@@ -5,3 +5,8 @@ import axiosInstance from "../utils/interceptors/axios-interceptor";
 export async function registerUser(normalizedUser: User) {
   return await axiosInstance.post("/users/register", normalizedUser);
 }
+
+//Get user by id
+export function getUserById(id: string) {
+  return axiosInstance.get(`/users/${id}`);
+}
---
+++ frontend/fullstack-react/src/utils/ui/alert.ts
@@ -0,0 +1,29 @@
+import { Bounce, toast } from "react-toastify";
+
+export function successMessage(message: string) {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+}
+
+export function errorMessage(message: string) {
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    transition: Bounce,
+  });
+}
