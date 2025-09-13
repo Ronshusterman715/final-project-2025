67b9d027967b280df0aa641c145864432cda26b4
Ronshusterman715 <ron7892123@gmail.com>
2025-09-13 14:29:07 +0300

    add  ToastContainer to app.tsx

--- frontend/fullstack-react/src/App.tsx
+++ frontend/fullstack-react/src/App.tsx
@@ -3,17 +3,23 @@ import Clubs from "./components/Clubs";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* TODO: create main page based on the instructions make its path "/" and change the clubs route to path "/clubs" */}
-        <Route path="/" element={<Clubs />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer />
+      <div className="container">
+        <Router>
+          <Routes>
+            {/* TODO: create main page based on the instructions make its path "/" and change the clubs route to path "/clubs" */}
+            <Route path="/" element={<Clubs />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
