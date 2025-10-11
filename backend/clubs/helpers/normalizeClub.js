987ec8a4f9f1a22f4ac61a614852811f6091d757
Ronshusterman715 <ron7892123@gmail.com>
2025-10-11 22:03:55 +0300

    fix static default image 3

--- backend/clubs/helpers/normalizeClub.js
+++ backend/clubs/helpers/normalizeClub.js
@@ -4,7 +4,7 @@ const normalizeClub = (rawClub, userId) => {
         ...rawClub,
         image: {
             //TODO: change default image when decide which club
-            url: rawClub.image.url || "/public/chatgpt-general-club-Image.png",
+            url: rawClub.image.url || "/images/chatgpt-general-club-Image.png",
             alt: rawClub.image.alt || `${rawClub.name} club image`,
         },
         userId: rawClub.user_id || userId,
